5b5c6f768aace773373ae17de9e9930764e9fe2b
kimyongghwa <coona335@gmail.com>
2021-03-21 14:09:02 +0900

    protobuf add setting

--- Common/protoc-3.12.3-win64/bin/GenProto.bat
+++ Common/protoc-3.12.3-win64/bin/GenProto.bat
@@ -2,7 +2,7 @@ protoc.exe -I=./ --csharp_out=./ ./Protocol.proto
 IF ERRORLEVEL 1 PAUSE
 
 START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./Protocol.proto
-XCOPY /Y Protocol.cs "../../../Client/Assets/Scripts/Packet"
+XCOPY /Y Protocol.cs "../../../DayDreamSlay/DayDreamSlay/Assets/Scripts/Packet"
 XCOPY /Y Protocol.cs "../../../Server/Server/Packet"
-XCOPY /Y ClientPacketManager.cs "../../../Client/Assets/Scripts/Packet"
+XCOPY /Y ClientPacketManager.cs "../../../DayDreamSlay/DayDreamSlay/Assets/Scripts/Packet"
 XCOPY /Y ServerPacketManager.cs "../../../Server/Server/Packet"
